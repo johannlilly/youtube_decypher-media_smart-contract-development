7d4c6784a16e7fb8c874a2b4f0b05a101453f35b
jlilly <johannlilly@users.noreply.github.com>
2018-03-30 16:26:16 +0200

    Solidity: struct, array

--- peoplelist/contracts/People.sol
+++ peoplelist/contracts/People.sol
@@ -1,4 +1,12 @@
 contract People {
 
+	// define an array that will contain the Person structs
+	Person[] public people;
+
+	struct public Person {
+		string firstName;
+		string lastName;
+		uint age;
+	}
 
 }
