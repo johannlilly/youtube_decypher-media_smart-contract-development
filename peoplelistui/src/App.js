fb8a2c39ea7f4871e6b3bf6e5a459f7d43bb6b6e
jlilly <johannlilly@users.noreply.github.com>
2018-03-30 23:28:48 +0200

    Define arrays that will hold data from contract

--- peoplelistui/src/App.js
+++ peoplelistui/src/App.js
@@ -29,6 +29,11 @@ class App extends Component {
     // store a javascript object that stores the state of your component
     // render() takes state as an argument, which tells the app how to render based on state
     // changing state automatically causes re-render to reflect the new state of the UI
+    this.state = {
+      firstNames: [],
+      lastNames: [],
+      ages: []
+    }
   }
   componentWillMount() {
     console.log(ETHEREUM_CLIENT)
