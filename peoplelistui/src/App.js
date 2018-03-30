1bcada65f4d2bf0efec48031f1f29ce4d66c8a1b
jlilly <johannlilly@users.noreply.github.com>
2018-03-30 23:10:19 +0200

    Run functions against contract object

--- peoplelistui/src/App.js
+++ peoplelistui/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Web3 from 'web3'
 
 var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
-
+// these can be added through input fields
 var peopleContractABI = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"people","outputs":[{"name":"firstName","type":"bytes32"},{"name":"lastName","type":"bytes32"},{"name":"age","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_firstName","type":"bytes32"},{"name":"_lastName","type":"bytes32"},{"name":"_age","type":"uint256"}],"name":"addPerson","outputs":[{"name":"added","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPeople","outputs":[{"name":"","type":"bytes32[]"},{"name":"","type":"bytes32[]"},{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"}]
 var peopleContractAddress = '0x7c04859ff79d872f3128545fd2a63d47fccb9265'
 
@@ -15,8 +15,13 @@ var peopleContractAddress = '0x7c04859ff79d872f3128545fd2a63d47fccb9265'
 // ETHEREUM_CLIENT.eth.contract(peopleContractABI).at(peopleContractAddress)
 // ^^^ this is the actual contract object
 // it is like truffle's People.deployed().then(function(instance){});
-// this means you can now store it as an oject
+// this means you can now store it as an object
+// this means you can call functions on it
+// this includes functions created in Solidity for the contract
 
+var peopleContract = ETHEREUM_CLIENT.eth.contract(peopleContractABI).at(peopleContractAddress);
+console.log(peopleContract.getPeople())
+console.log(peopleContract.getPeople())
 
 class App extends Component {
   componentWillMount() {
