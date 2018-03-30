bfa5bf47ea4d865b3135d89730e06e01a0463110
jlilly <johannlilly@users.noreply.github.com>
2018-03-30 17:27:53 +0200

    function getPeople()
    Create a new Person structure in memory. Populate it with the element of the array in focus. Feed the properties of the structure from memory into an output array

--- peoplelist/contracts/People.sol
+++ peoplelist/contracts/People.sol
@@ -10,8 +10,8 @@ contract People {
 	}
 
 	// using 'returns' is helpful 
-	// a function can take a string and convert from bytecode for us
-	function addPerson(string _firstName, string _lastName, uint _age) returns (bool added) {
+	// a function can take a string and convert from bytecode for us, but we are going to specify bytes32
+	function addPerson(bytes32 _firstName, bytes32 _lastName, uint _age) returns (bool added) {
 
 		// EVM has memory to store state variables
 		// create struct in memory before adding to array
@@ -34,7 +34,25 @@ contract People {
 	// return an array of strings (firstName, lastName), and an array of ages.
 	function getPeople() constant returns (bytes32[],bytes32[],uint[]) {
 		// creating an array of strings can be an issue if they are of different lengths.
-		// change strings to a fixed length and pad with spaces.
+		// change strings to a fixed length and pad with spaces => bytes32
+
+
+		bytes32[] firstNames;
+		bytes32[] lastNames;
+		uint[] ages;
+
+		for (uint i=0; i < people.length; i++) {
+			// create a new Person structure in memory
+			// populate it with the element of the array in focus
+			// feed the properties of the structure from memory into an output array
+			Person memory currentPerson;
+			currentPerson = people[i];
+
+			firstNames.push(currentPerson.firstName);
+			lastNames.push(currentPerson.lastName);
+			ages.push(currentPerson.age);
+		}
+
 	}
 
 }
